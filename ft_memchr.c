d7993494b94146edb758361d54a714b146c3e581
ambroiselebs <amberenger06@gmail.com>
2024-09-05 21:28:11 +0200

    FIXED ft_memchr ft_strncmp

--- ft_memchr.c
+++ ft_memchr.c
@@ -15,9 +15,7 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	char	*str;
-	int		i;
 
-	i = 0;
 	str = (char *)s;
 	while (n-- > 0)
 	{
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while (s1[i] && s2[i] && i < (int) n)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
